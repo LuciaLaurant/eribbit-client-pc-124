5e02ddbce69a009c4e18136a25c7ff73f7a87da0
LuciaXu <3012170145@qq.com>
2022-10-27 16:53:48 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <p>456</p>
+  <div id="app">
+    123
+    <p>{{store.state.username}}</p>
+  </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import {
+  useStore
+} from 'vuex'
+export default {
+  name: 'App',
+  setup() {
+    const store = useStore()
+    // console.log(store.state.username)
+    return { store }
   }
 }
+</script>
+
+<style lang="less">
+
 </style>
